0fb4471acb680c152114894e05cef6fb6aa69375
sash <na-okne@mail.ru>
2021-12-13 23:53:09 +0300

    44

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import MyPosts from "./MyPosts";
 import {addPostActionCreator, updateNewPostTextActionCreator} from '../../../redux/profile-reducer'
+import StoreContext from "../../../store-context";
+import store from "../../../redux/redux-store";
 
 const MyPostsContainer = (props) => {
-	let state = props.store.getState();
-
-	let onPostChange = (text) => {
-		let action = updateNewPostTextActionCreator(text)
-		props.store.dispatch(action)
-	}
-
-	let addPost = () => {
-		let action = addPostActionCreator()
-		props.store.dispatch(action)
-	}
+	//let state = props.store.getState();
 
 	
-
 	return (
-		<MyPosts updateNewPostText={onPostChange} addPost={addPost} posts={state.profilePage.posts} newPostText={state.profilePage.newPostText}/>
+		<StoreContext.Consumer>
+			{ (store) => {
+				let state = props.store.getState();
+				let onPostChange = (text) => {
+					let action = updateNewPostTextActionCreator(text)
+					props.store.dispatch(action)
+				}
+			
+				let addPost = () => {
+					let action = addPostActionCreator()
+					props.store.dispatch(action)
+				}
+				return (
+					<MyPosts updateNewPostText={onPostChange} 
+			addPost={addPost} 
+			posts={state.profilePage.posts} 
+			newPostText={state.profilePage.newPostText}/>
+				)
+			}
+			
+			
+			}
+			</StoreContext.Consumer>
+		
 	)
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './redux/redux-store';
+import StoreContext from './store-context';
 
 let rerender = (state) => {
 	ReactDOM.render(
-		<App state={state} dispatch={store.dispatch.bind(store)} store={store}/>,
+		<StoreContext.Provider value={store}>
+			<App />
+		</StoreContext.Provider>
+		,
 	 document.getElementById('root')
   );
 }
