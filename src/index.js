054da59cc08c90bd0dfdd7c8ce5184ee2b921aee
sash <na-okne@mail.ru>
2022-03-12 12:36:37 +0300

    44

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store';
 import {Provider} from 'react-redux';
+import StoreContext from './StoreContext';
 
 
 	ReactDOM.render(
-		<Provider store={store}>
+		<StoreContext.Provider value={store}>
 			<App />
-		</Provider>
+		</StoreContext.Provider>
 		,
 	 document.getElementById('root')
   );
